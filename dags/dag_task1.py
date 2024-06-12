1ae954fa95b0f193eb66f8451244c07ec6c79e03
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2024-06-12 12:26:22 +0200

    legger til tid og random tall i eksempel-task

--- dags/dag_task1.py
+++ dags/dag_task1.py
@@ -1,7 +1,11 @@
 # test task
 import time
+import random
 
 print("Hello from task1!")
-# wait 5 seconds
-time.sleep(5)
+time.sleep(2)
+print(time.ctime())
+time.sleep(2)
+print(random.randint(1, 100))
+time.sleep(2)
 print("Task1 done!")
